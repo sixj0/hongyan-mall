332762efbcb48db2deb331bfea4aa1f7e77ba784
sixj <sixj@runlion.com>
2022-02-17 10:11:23 +0800

    创建项目

---
+++ src/main/java/com/hongyan/mall/HongyanMallApplication.java
@@ -0,0 +1,17 @@
+package com.hongyan.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author sixj
+ */
+@SpringBootApplication
+public class HongyanMallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HongyanMallApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hongyan/mall/HongyanMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongyan.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongyanMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
